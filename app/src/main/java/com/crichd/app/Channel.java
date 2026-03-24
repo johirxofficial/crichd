495744858fb42388f1ea1421ba58f28dc085849c
johirxofficial <johirxofficial@gmail.com>
2026-03-24 12:11:18 +0600

    Create Channel.java

---
+++ app/src/main/java/com/crichd/app/Channel.java
@@ -0,0 +1,9 @@
+package com.crichd.app;
+
+public class Channel {
+    public String name;
+    public String logoUrl;
+    public String streamUrl;
+    public String referrer;
+    public String origin;
+}
