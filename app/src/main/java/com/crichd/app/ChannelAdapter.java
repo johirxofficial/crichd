1eab243d952f32c2269d0dbf96871d06c132b446
johirxofficial <johirxofficial@gmail.com>
2026-03-24 12:11:55 +0600

    Create ChannelAdapter.java

---
+++ app/src/main/java/com/crichd/app/ChannelAdapter.java
@@ -0,0 +1,60 @@
+package com.crichd.app;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import java.util.List;
+
+public class ChannelAdapter extends RecyclerView.Adapter<ChannelAdapter.ViewHolder> {
+    private List<Channel> channels;
+    private OnChannelClickListener listener;
+
+    public interface OnChannelClickListener {
+        void onChannelClick(Channel channel);
+    }
+
+    public ChannelAdapter(List<Channel> channels, OnChannelClickListener listener) {
+        this.channels = channels;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_channel, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Channel channel = channels.get(position);
+        holder.name.setText(channel.name);
+        
+        Glide.with(holder.itemView.getContext())
+             .load(channel.logoUrl)
+             .placeholder(android.R.drawable.ic_menu_camera)
+             .into(holder.logo);
+
+        holder.itemView.setOnClickListener(v -> listener.onChannelClick(channel));
+    }
+
+    @Override
+    public int getItemCount() {
+        return channels.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView logo;
+        TextView name;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            logo = itemView.findViewById(R.id.channel_logo);
+            name = itemView.findViewById(R.id.channel_name);
+        }
+    }
+}
